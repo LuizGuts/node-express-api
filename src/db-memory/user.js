6694912ca3e096d12dfc9415757acfec936f8c7b
LuizGuts <luiz.alves.1@outlook.com>
2024-02-15 22:15:07 -0300

    Add GET e POST em /user

---
+++ src/db-memory/user.js
@@ -0,0 +1,20 @@
+export const users = [
+    {
+        id: 1,
+        name: 'Luiz Gustavo Santos',
+        email: 'luiz.gustavo@gmail.com',
+        avatar: 'https://steamuserimages-a.akamaihd.net/ugc/32995786825512338/52FA74FF52603C1B211C1D0D1012B88F09D2D36E/?imw=660&imh=1024&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true'
+    },
+    {
+        id: 2,
+        name: 'Lucas Laidens',
+        email: 'lucas.laidens@gmail.com',
+        avatar: 'https://i.ytimg.com/vi/kGMlQb0WAok/hqdefault.jpg'
+    },
+    {
+        id: 3,
+        name: 'Eduardo Medeiros',
+        email: 'eduardo.medeiros@gmail.com',
+        avatar: 'https://media.gettyimages.com/id/172387616/pt/foto/um-hart-cervo-veado-macho.jpg?s=612x612&w=gi&k=20&c=NvK4hcYL6PsS6tCF0DwnHHnOA4pE0yCXDBK0zbiHuW4='
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,15 +1,37 @@
 //const express = require('express')
 import express from 'express'
-const app = express()
-const port = 3000
+import {PORT, HOST} from './config.js'
+import {users} from './db-memory/user.js'
 
-app.get('/', (req, res) => {
-  res.json({message: 'Nada demais aqui'})
+const app = express()
+
+app.use(express.json()) 
+
+  app.get('/', (req, res) => {
+  res.json({message: "Nada demais aqui"})
 })
-app.get('/segredo', (req, res) => {
-    res.json({message: 'Eu vo ti conta um segredo >:D'})
+
+  app.get('/segredo', (req, res) => {
+    res.json({message: "Eu vo ti conta um segredo >:D"})
   })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  app.get('/user', (req, res) => {
+    res.json({
+      success: "Usuarios Listados com Sucesso",
+      users
+    })
+  })
+
+  app.post('/user', (req, res) => {
+    const user = req.body
+    user.id = users[users.length - 1].id + 1
+    users.push(user)
+    console.log(user)
+    res.json({
+      success: "Usuarios Listados com Sucesso",
+      users
+    })
+  })
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${HOST}:${PORT}`)
 })
