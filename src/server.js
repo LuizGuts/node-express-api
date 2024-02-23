1fe8de19ce5cadec1b1754b2ac70ad6d3685fb01
LuizGuts <luiz.alves.1@outlook.com>
2024-02-22 22:33:31 -0300

    22/02

--- src/server.js
+++ src/server.js
@@ -2,14 +2,18 @@
 import express from 'express'
 import {PORT, HOST} from './config.js'
 import {users} from './db-memory/user.js'
+import logger from './middlewares/logger.js'
 
-const app = express()
+  const app = express()
 
-app.use(express.json()) 
+//middleware
+  app.use(express.json()) 
+  app.use('/user', logger)
 
+//routes
   app.get('/', (req, res) => {
-  res.json({message: "Nada demais aqui"})
-})
+    res.json({message: "Nada demais aqui"})
+  })
 
   app.get('/segredo', (req, res) => {
     res.json({message: "Eu vo ti conta um segredo >:D"})
@@ -32,6 +36,36 @@ app.use(express.json())
       users
     })
   })
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${HOST}:${PORT}`)
-})
+
+  app.delete('/user', (req, res) => {
+    const id = req.body.id
+    const usersResult = users.filter(user => user.id !== id)
+    res.json({
+      success: `Usuário ${id} removido com sucesso!`,
+      usersResult
+    })
+  })
+
+  app.put('/user', (req, res) => {
+    const newUser = req.body
+    const usersResult = users.map(user => {
+      if(user.id === newUser.id){
+        return {
+          id: user.id,
+          name: newUser.name || user.name,
+          email: newUser.email || user.email,
+          avatar: newUser.avatar || user.avatar
+        }
+      }
+        return user
+    })
+    res.json({
+      success: `Usuario ${newUser.id} atualizado com sucesso`,
+      usersResult
+    })
+  })
+
+  //run server
+  app.listen(PORT, () => {
+    console.log(`Servidor Rodando em ${HOST}:${PORT}`)
+  })
